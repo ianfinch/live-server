14fe3698be66890769ab05fbc4766863986b075a
Ian Finch <ianfinch@users.noreply.github.com>
2026-08-25 00:01:28 +0100

    Fixed bug where page title was picked up from the sidebar, because it was the first h1 on the page

--- content/markdown.js
+++ content/markdown.js
@@ -161,15 +161,19 @@ const convertMarkdown = async (converter) => {
         handleFrontmatter(converter.getMetadata());
     });
 
-    // Update the title with the first h1 on the page
-    const headers = document.getElementsByTagName("h1");
-    if (headers.length > 0) {
+    // Update the title with the first h1 in the main content
+    const contentElems = document.getElementsByClassName("content");
+    if (contentElems.length > 0) {
 
-        const headerText = headers[0].textContent;
-        const title = document.getElementsByTagName("title");
-        if (title.length > 0) {
+        const headers = contentElems[0].getElementsByTagName("h1");
+        if (headers.length > 0) {
 
-            title[0].textContent = headerText;
+            const headerText = headers[0].textContent;
+            const title = document.getElementsByTagName("title");
+            if (title.length > 0) {
+    
+                title[0].textContent = headerText;
+            }
         }
     }
 
